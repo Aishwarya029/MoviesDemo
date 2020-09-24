ba452bc7c2923822a366187ce89988ebca4840fc
Aishwarya <ashnayak029@gmail.com>
2020-09-24 20:16:13 +0530

    ch

--- moviesql/moviequery.sql
+++ moviesql/moviequery.sql
@@ -28,12 +28,12 @@ insert into category values("1","Batman","batmanhome.jpg","GOTHAM","batmansymbol
 insert into category values("2","HarryPotter","howarts-houses.jpg","HOGWARDS","hogbag.jpg");
 
 insert into movie values("1","1","BM1","batman1989.jpg","Batman (1989)");
-insert into movie values("2","1","BM2","BatmanReturns(1992).jpg","Batman Returns (1992)");
-insert into movie values("3","1","BM3","BatmanForever(1995).jpg","Batman Forever (1995)");
-insert into movie values("4","1","BM4","Batman&Robin(1997).jpg","Batman & Robin (1997)");
-insert into movie values("5","1","BM5","BatmanBegins(2005).jpg","Batman Begins (2005)");
-insert into movie values("6","1","BM6","TheDarkKnight(2008).jpg","The Dark Knight (2008)");
-insert into movie values("7","1","BM7","TheDarkKnightRises(2012).jpg","The Dark Knight Rises (2012)");
+insert into movie values("2","1","BM2","BatmanReturns(1993).jpg","Batman Returns (1993)");
+insert into movie values("3","1","BM3","BatmanForever(1996).jpg","Batman Forever (1996)");
+insert into movie values("4","1","BM4","Batman&Robin(1998).jpg","Batman & Robin (1998)");
+insert into movie values("5","1","BM5","BatmanBegins(2006).jpg","Batman Begins (2006)");
+insert into movie values("6","1","BM6","TheDarkKnight(2009).jpg","The Dark Knight (2009)");
+insert into movie values("7","1","BM7","TheDarkKnightRises(2013).jpg","The Dark Knight Rises (2013)");
 insert into movie values("8","1","BM8","BatmanvSuperman.jpg","Batman v Superman(2016)");
 insert into movie values("9","1","BM9","thebatman.jpg","The Batman (2021)");
 insert into movie values("10","2","HP1","HarryPotterandthePhilosopher'sStone.jpg","Harry Potter and the Philosopher's Stone");
@@ -42,8 +42,8 @@ insert into movie values("12","2","HP3","HarryPotterandthePrisonerofAzkaban.jpg"
 insert into movie values("13","2","HP4","HarryPotterandtheGobletofFire.jpg","Harry Potter and the Goblet of Fire");
 insert into movie values("14","2","HP5","HarryPotterandtheOrderofthePhoenix.jpg","Harry Potter and the Order of the Phoenix");
 insert into movie values("15","2","HP6","HarryPotterandtheHalf-BloodPrince.jpg","Harry Potter and the Half-Blood Prince");
-insert into movie values("16","2","HP7","HarryPotterandtheDeathlyHallows–Part1.jpg","Harry Potter and the Deathly Hallows – Part 1");
-insert into movie values("17","2","HP8","HarryPotterandtheDeathlyHallows–Part2.jpg","Harry Potter and the Deathly Hallows – Part 2");
+insert into movie values("16","2","HP7","HarryPotterandtheDeathlyHallowsPart1.jpg","Harry Potter and the Deathly Hallows – Part 1");
+insert into movie values("17","2","HP8","HarryPotterandtheDeathlyHallowsPart2.jpg","Harry Potter and the Deathly Hallows – Part 2");
 
 
 insert into result values("1","https://en.wikipedia.org/wiki/Batman_(1989_film)","https://www.youtube.com/watch?v=l_qlrjafOK0&feature=youtu.be","https://t.me/Movies_Batman/750","Batman (1989)");
